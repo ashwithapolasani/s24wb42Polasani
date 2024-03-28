b2a6828968d11a8534bc6aa67534d4d88bd43b3e
Ashwitha <polasaniashwithareddy@gmail.com>
2024-03-27 19:26:26 -0500

    Updated code

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var gridRouter = require('./routes/grid'); //grid endpoint added
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/grid', gridRouter); //grid endpoint added
+app.use('/pick', pickRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
